566102221d89b55d24763fca0f7f27a082e8a461
Mindaugas Urbontaitis <mindaugas@urbontatis.lt>
2017-01-25 20:45:39 +0000

    An example of swagger API documentation

--- rest-module/src/main/java/com/cycligo/backend/comment/CommentController.java
+++ rest-module/src/main/java/com/cycligo/backend/comment/CommentController.java
@@ -1,5 +1,7 @@
 package com.cycligo.backend.comment;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.hibernate.cfg.NotYetImplementedException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,6 +15,7 @@ import java.util.List;
  * Created by Mindaugas Urbontaitis on 24/01/2017.
  * cycligo-backend
  */
+@Api(value = "customers")
 @RestController
 public class CommentController {
 
@@ -23,11 +26,17 @@ public class CommentController {
         this.commentService = commentService;
     }
 
+    @ApiOperation(value = "Gets a comments based on parent id",
+            notes = "Retrieves a comments",
+            response = Comments.class)
     @RequestMapping(value = "/comments", method = RequestMethod.GET)
     Comments commentsByParentId(@RequestParam(value = "parentId") Long parentId) {
         return commentService.findByParentId(parentId);
     }
 
+    @ApiOperation(value = "Post a comment based on parent id",
+            notes = "Post a comment",
+            response = Object.class)
     @RequestMapping(value = "/comments/comment", method = RequestMethod.PUT)
     List<Object> postComment(@RequestParam(value = "parentId") Long parentId) {
         throw new NotYetImplementedException("TODO implement comments API");
