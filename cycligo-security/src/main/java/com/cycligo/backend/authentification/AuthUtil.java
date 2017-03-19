860a63a220b7d75843024ef6e77ee8e50ebb7200
Mindaugas Urbontaitis <mindaugas@urbontatis.lt>
2017-03-19 21:35:29 +0000

    Removed security module

--- cycligo-security/src/main/java/com/cycligo/backend/authentification/AuthUtil.java
+++
@@ -1,8 +0,0 @@
-package com.cycligo.backend.authentification;
-
-/**
- * Created by Mindaugas Urbontaitis on 27/01/2017.
- * cycligo-backend
- */
-public class AuthUtil {
-}
--- cycligo-security/src/main/java/com/cycligo/backend/config/SocialConfiguration.java
+++
@@ -1,8 +0,0 @@
-package com.cycligo.backend.config;
-
-/**
- * Created by Mindaugas Urbontaitis on 27/01/2017.
- * cycligo-backend
- */
-public class SocialConfiguration {
-}
