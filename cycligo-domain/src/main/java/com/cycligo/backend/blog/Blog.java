ccbd00513e689668d959365dc3f6386581da9c28
Mindaugas Urbontaitis <mindaugas@urbontatis.lt>
2017-04-12 22:21:04 +0300

    MVP82 Add Blog and Tag entities.

---
+++ cycligo-domain/src/main/java/com/cycligo/backend/blog/Blog.java
@@ -0,0 +1,151 @@
+package com.cycligo.backend.blog;
+
+import com.cycligo.backend.lookup.LookupValue;
+import com.cycligo.backend.tag.Tag;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.Set;
+
+/**
+ * Created by Mindaugas Urbontaitis on 12/04/2017.
+ * cycligo-rest-api
+ */
+@Entity
+public class Blog {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+
+    @Lob
+    @Column(length = 16777215, columnDefinition = "MEDIUMTEXT")
+    private String content;
+
+    private Long photoId;
+
+    @OneToOne(fetch = FetchType.LAZY, cascade={CascadeType.PERSIST, CascadeType.MERGE})
+    @JoinColumn(name = "category_id", nullable = false)
+    private LookupValue category;
+
+    private Boolean approved;
+
+    @ManyToMany(fetch = FetchType.LAZY, cascade={CascadeType.PERSIST, CascadeType.MERGE})
+    @JoinTable(name = "blog_tags",
+            joinColumns = {
+                @JoinColumn(name = "blog_id", nullable = false, updatable = false)
+            },
+            inverseJoinColumns = {
+                @JoinColumn(name = "tag_id", nullable = false, updatable = false)
+            })
+    private Set<Tag> tags;
+
+    private Long createdBy;
+
+    private LocalDateTime createdAt;
+
+    private Long updatedBy;
+
+    private LocalDateTime updatedAt;
+
+    @Version
+    private int version;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Long getPhotoId() {
+        return photoId;
+    }
+
+    public void setPhotoId(Long photoId) {
+        this.photoId = photoId;
+    }
+
+    public LookupValue getCategory() {
+        return category;
+    }
+
+    public void setCategory(LookupValue category) {
+        this.category = category;
+    }
+
+    public Boolean getApproved() {
+        return approved;
+    }
+
+    public void setApproved(Boolean approved) {
+        this.approved = approved;
+    }
+
+    public Set<Tag> getTags() {
+        return tags;
+    }
+
+    public void setTags(Set<Tag> tags) {
+        this.tags = tags;
+    }
+
+    public Long getCreatedBy() {
+        return createdBy;
+    }
+
+    public void setCreatedBy(Long createdBy) {
+        this.createdBy = createdBy;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Long getUpdatedBy() {
+        return updatedBy;
+    }
+
+    public void setUpdatedBy(Long updatedBy) {
+        this.updatedBy = updatedBy;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(LocalDateTime updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    public int getVersion() {
+        return version;
+    }
+
+    public void setVersion(int version) {
+        this.version = version;
+    }
+}
